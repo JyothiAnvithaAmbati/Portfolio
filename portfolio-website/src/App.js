31cdf84f14feabcc6fc682faff5b43ae8a65bbb6
Jyothi Anvitha Ambati <ajanvitha27@gmail.com>
2024-08-24 00:04:07 -0400

    Add files via upload

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
